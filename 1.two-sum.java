b31b5ec98e5d4ce5ccbffec22115fab0d20ea44c
ZhenhaiPENG <peng.zhenh@northeastern.edu>
2025-11-06 17:31:35 -0500

    Initial commit of existing project files

---
+++ 1.two-sum.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
